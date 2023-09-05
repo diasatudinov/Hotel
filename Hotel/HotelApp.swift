8eefc7a345c0a74e4dabf415b5e9c7362693954d
Dias <dias.atudinov@gmail.com>
2023-09-05 11:23:43 +0600

    Initial Commit

---
+++ Hotel/HotelApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotelApp.swift
+//  Hotel
+//
+//  Created by Atudinov Dias on 05.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HotelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
